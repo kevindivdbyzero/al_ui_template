292dc5a6b71daad7b22cb002186c99f96fd43962
andresfaq <andres.artunduaga@softpowering.com>
2016-04-04 00:20:07 -0500

    Carousel created, (Script fail)

--- src/main/mfinder/partials/remoteImageLoader/RemoteImageLoader.js
+++ src/main/mfinder/partials/remoteImageLoader/RemoteImageLoader.js
@@ -18,11 +18,13 @@ define( [ 'angular',
           'config/config' ],
     function( angular, config ) {
         "use strict";
+    
+      
 
         var RemoteImageLoader = function( $scope ) {
-
             var config  = angular.module("config");
             $scope.view = {images: config.apiImg};
+            
             
         };
 
