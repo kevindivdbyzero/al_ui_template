a30a95ac72644f8532f648378bc3834641ef8f4e
andresfaq <andres@andres.com>
2016-05-11 15:39:50 -0500

    fixing duplicated movies in lastVisited list

--- src/main/mfinder/services/AppStateService.js
+++ src/main/mfinder/services/AppStateService.js
@@ -26,16 +26,37 @@ define( ['angular',
 
 
             
-        var lastVisited = [10];
+        var lastVisited = [];
 
 
+         var searchVisited = function (entity) {
+
+             var visited = false;
+
+             var val = entity.title;
+
+             for(var i = 0; i<lastVisited.length; i++){
+                 var item = lastVisited[i].title;
+                 if(item === val){
+                     visited = true;
+                     break;
+                 }
+             }
+
+             return visited;             
+         };   
+
+            
 
         self.setLastVisited = function(entity){
 
-            if(listOf(entity)){
-                lastVisited.push(entity);    
+            if( searchVisited(entity) ) {
+                console.log("The movie is already in the list ========================================================");
+            }else{
+                console.log("Storing movie the movie :", entity.title);
+                lastVisited.push(entity);
             }
-            
+
         };
 
         self.getLastVisitedList = function(){
